811191e7065b14c37e1fb106ea6f46b04b4ded4a
R K S <ramakantasahoo1202@gmail.com>
2024-06-06 10:44:20 +0530

    updated

--- server.py
+++ server.py
@@ -28,6 +28,7 @@ def recommend():
 
 
 # This should not be in production
-# if __name__ == '__main__':
-#     app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
 
